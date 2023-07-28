aab6a6a459fd57b525ade5edb95757266ff9ed08
Denys Lenskyi <denislensky94@gmail.com>
2023-07-28 16:15:36 -0400

    deploy stage added to gitlab-ci.yml

--- src/main/java/ua/foxminded/javaspring/lenskyi/university/controller/HomeController.java
+++ src/main/java/ua/foxminded/javaspring/lenskyi/university/controller/HomeController.java
@@ -1,20 +1,15 @@
 package ua.foxminded.javaspring.lenskyi.university.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import ua.foxminded.javaspring.lenskyi.university.repository.UserRepository;
 
 @Controller
 @RequestMapping("/")
 public class HomeController {
 
-    @Autowired
-    UserRepository userRepo;
-
-    @GetMapping("/")
+    @GetMapping
     public String home(Model model) {
         return "index";
     }

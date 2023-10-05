4db74e3b847f29a75def3e1e98e8c9e8f55eec99
Denys Lenskyi <denislensky94@gmail.com>
2023-10-05 08:14:43 -0400

    BaseRepository added, existByName() method moved into it

--- src/main/java/ua/foxminded/javaspring/lenskyi/university/repository/GroupRepository.java
+++ src/main/java/ua/foxminded/javaspring/lenskyi/university/repository/GroupRepository.java
@@ -7,9 +7,7 @@ import ua.foxminded.javaspring.lenskyi.university.model.Group;
 import java.util.Optional;
 
 @Repository
-public interface GroupRepository extends JpaRepository<Group, Long> {
-
-    boolean existsByName(String groupName);
+public interface GroupRepository extends JpaRepository<Group, Long>, BaseRepository<Group> {
 
     Optional<Group> findByName(String groupName);
 }

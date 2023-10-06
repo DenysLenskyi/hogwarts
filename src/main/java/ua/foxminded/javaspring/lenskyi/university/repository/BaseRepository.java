7c7ce219866d694c7e7adeeda44c10489e6e5ff3
yushin <yushin.khpi@gmail.com>
2023-10-06 12:20:41 +0300

    experiments with BaseRepository

--- src/main/java/ua/foxminded/javaspring/lenskyi/university/repository/BaseRepository.java
+++ src/main/java/ua/foxminded/javaspring/lenskyi/university/repository/BaseRepository.java
@@ -1,7 +1,9 @@
 package ua.foxminded.javaspring.lenskyi.university.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.NoRepositoryBean;
 
+@NoRepositoryBean
 public interface BaseRepository<T, ID> extends JpaRepository<T, ID> {
 
     boolean existsByName(String name);

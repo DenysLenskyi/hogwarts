5154662c369c9aa4a2bc39a7dc261463f8dc116c
Denys Lenskyi <denislensky94@gmail.com>
2023-10-13 12:00:06 -0400

    templates updated

--- src/main/java/ua/foxminded/javaspring/lenskyi/university/controller/HomeController.java
+++ src/main/java/ua/foxminded/javaspring/lenskyi/university/controller/HomeController.java
@@ -11,13 +11,19 @@ public class HomeController {
         return "index";
     }
 
-    @GetMapping("/about")
-    public String about() {
-        return "about";
+    @GetMapping("/logged-user-info")
+    public String loggedUserPage() {
+        return "logged-user-info";
     }
 
     @GetMapping("/contacts")
     public String contacts() {
         return "contacts";
     }
+
+    @GetMapping("/about")
+    public String about() {
+        return "about";
+    }
+
 }
